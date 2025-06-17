022e5c5f56e0b30fbe218f3f2e479716fcc32a2a
gJatin <gjatin278@gmail.com>
2025-06-17 21:05:58 +0530

    Written code in JS File

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const API=`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qrText)}&size=200x200`;
+function GenerateQR() {
+    const qrText = document.querySelector("input").value.trim();
+
+    if(!qrText){
+        alert("enter valid Text or URL");
+        return;
+    }
+    
+    const Image = document.querySelector("img");
+    Image.src = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qrText)}&size=200x200`;
+}
