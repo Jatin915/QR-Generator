b59085eed20986e2a008b994e7cc4082d58fe860
gJatin <gjatin278@gmail.com>
2025-06-18 17:11:11 +0530

    Added Download and Mode Change Featues

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 function GenerateQR() {
     const qrText = document.querySelector("input").value.trim();
 
-    if(!qrText){
-        alert("enter valid Text or URL");
-        return;
-    }
-    
+    if(!qrText) return alert("enter valid Text or URL");
+
     const Image = document.querySelector("img");
     Image.src = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qrText)}&size=200x200`;
+
+let downloadBtn=document.querySelector("#download");
+let Download = document.querySelector("a");
+Download.href="`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qrText)}&size=200x200`";
+downloadBtn.style.display = "inline-block";
+
+}
+
+function modeChange(){
+    document.body.classList.toggle("dark");
 }
